c2cd6c4313dec7885a225437c7a4ba6e0466b41d
HoaNguyen2005 <hoab2308359@student.ctu.edu.vn>
2026-06-02 22:16:38 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const contactRoutes = require('./app/routes/contact.route');
 const express = require('express');
 const cors = require('cors');
+const ApiError = require('./app/api-error');
 
 const app = express();
 
@@ -8,6 +10,18 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to contact book application.' });
+});
+
+app.use("/api/contacts", contactRoutes);
+
+//handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({ message: 
+    err.message || 'Internal Server Error',
+   });
 });
 
 module.exports = app;
